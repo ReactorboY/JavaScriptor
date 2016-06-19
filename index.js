90b4327614b2ad534157d500fe4b503230e1d277
Muhammad Hussain <samsteve124@gmail.com>
2016-06-19 14:19:05 +0530

    initial update

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
 var express = require('express');
 
 var app = express();
-
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public' ));
 // homepage
 app.get('/', function (req, res) {
    res.send('Hello World');
 });
-
-// about page
-app.get('/about', function (req, res) {
-   res.send('This is About');
-});
-
-// string to match abcd
-app.get('/ab?cd', function(req, res) {
-  res.send('ab?cd');
-});
-// custom 404 pages
-app.use(' ', function (req, res) {
-   res.send('Not found');
-});
-
 
 app.listen(3000, function () {
    console.log('Server is listening to port 3000');
