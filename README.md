bea30818b42b131776a0ebac7e22519b193541f9
Muhammad Hussain <samsteve124@gmail.com>
2016-06-21 22:00:30 +0530

    initial

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # JavaScriptor
 learn JavaScript while doing it on real apps.
+
+## Things Needed
+1. NodeJs
+2. text Editor
+3. Patience
--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-var express = require('express');
-var ehbs = require('express-handlebars');
 
-
-var app = express();
-app.engine('handlebars', ehbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
-app.use(express.static(__dirname + '/public' ));
-// homepage
-app.get('/', function (req, res) {
-   res.render('home');
-});
-
-app.listen(3000, function () {
-   console.log('Server is listening to port 3000');
-})
